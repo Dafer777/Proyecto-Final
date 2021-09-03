1d623c2bc15de0c3e06bb593d74e0cc915342bb8
David Fernandez <isaacdavid959@gmail.com>
2021-09-03 10:20:38 -0600

    Primera Parte

---
+++ ProyectoFinal/InicioSesion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProyectoFinal
+{
+    class InicioSesion
+    {
+        public static void Inicio()
+        {
+            int i = 0;
+            //usuario nivel 1 ADMIN
+            Console.WriteLine("Ingrese el usuario");
+            string usuario = Console.ReadLine();
+            Console.WriteLine("Ingrese la contraseña");
+            string contraseña = Console.ReadLine();
+
+            if ((usuario == "admin") && (contraseña == "123"))
+            {
+                i = 5;
+                Console.WriteLine("Ha ingresado exitosamente");
+            }
+            else if (i > 3)
+
+
+                Console.WriteLine("El numero de intento a sido alcanzado.");
+  
+
+
+            else
+            {
+                Console.WriteLine("El nombre de usuario o contraseña son incorrctos");
+                i++;
+
+
+                //usuario nivel 2
+
+                // el acceso de usuario nivel 2 lo tienes que crear tu Gabriel
+                Console.WriteLine("Ingrese el usuario");
+
+                Console.ReadKey();
+           
+            }
+
+        }
+
+
+
+    }
+}
---
+++ ProyectoFinal/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ProyectoFinal;
+
+namespace ProyectoFinal
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            InicioSesion.Inicio();
+            bool showMenu = true;
+
+            while (showMenu)
+            {
+                showMenu = Menu(); //llamado al metodo Menu()
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+        private static bool Menu()
+        {
+            //USUARIO ADMIN
+            Console.Write("Nombre del usuario: ");
+            string name = Console.ReadLine();
+            Console.Write("Contraseña: ");
+            string contraseña = Console.ReadLine();
+
+
+            int i = 0;
+
+            if ((name == "admin") && (contraseña == "123"))
+            {
+                Console.WriteLine("Ha ingresado exitosamente");
+                i = 3;
+            }
+
+            //crear el menu para mostrar al usuario
+            //Console.Clear(); //permite limpiar la consola
+            Console.WriteLine("Seleccion la operación a realizar: ");
+            Console.WriteLine("1. Registrar nuevo usuario");
+            Console.WriteLine("2. Actualizar datos de paciente");
+            Console.WriteLine("3. Eliminar datos de paciente");
+            Console.WriteLine("4. Mostrar listado de paciente");
+            Console.WriteLine("5. Salir");
+            Console.Write("\nOpcion: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    register(); //llamado al metodo registrar
+                    return true;
+                case "2":
+                    
+                case "3":
+                
+                case "4":
+                  
+                    return true;
+                case "5":
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+
+
+
+        //metodo para obtener la ruta del archivo
+        private static string getPath()
+        {
+            string path = @"D:\ejemplo\registros.txt";
+            return path;
+        }
+
+        //metodo para registrar datos en el archivo
+        private static void register()
+        {
+            //solicitar los datos para registar al usuario
+            Console.WriteLine("DATOS USUARIO");
+            Console.Write("Nombre del usuario: ");
+            string name = Console.ReadLine();
+            Console.Write("Contraseña: ");
+            string contraseña = Console.ReadLine();
+
+            //crear el archivo, uso de StreamWriter para escribir el archivo
+            using (StreamWriter sw = File.AppendText(getPath()))
+            {
+                sw.WriteLine("{0}; {1}", name, contraseña);
+                sw.Close();
+            }
+        }
+
+    }
+}
